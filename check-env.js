99cfbe0bb2b5ab719f1108f47b9a1972c89c5b1e
Adrian Carolli <adrian.caarolli@gmail.com>
2022-09-28 10:10:33 -0400

    test

--- check-env.js
+++ check-env.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 
-const REQUIRED_ENVS = ["API_URL", "PRODUCTION"];
+const REQUIRED_ENVS = ["API_URL", "PRODUCTION", "PREVIEW", "DEVELOPMENT"];
 
 REQUIRED_ENVS.map((key) => {
+  let exit = false;
   const value = process.env[key];
   if (!value) {
     console.error(`Missing environment variable: ${key}`);
     console.error(
       `To fix this issue add it to your .env file locally or on the server`
     );
+    exit = true;
+  }
+
+  if (exit) {
     process.exit(1);
   }
 });
